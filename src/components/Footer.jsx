fed2cb54701f1d8c57964f24d74fb14502d9f6e9
Lukibwoy <lukaszwiczynski@gmail.com>
2025-01-08 20:24:58 +0100

    email update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,36 @@
 import Logo from './Logo'
 
 const Footer = () => {
-  return (
-    <div id="contact" className="relative text-white font-quicksand px-4 bg-green-900 border-t-[1px] -mt-40 md:mt-24">
-      {/* Logo w górnej części stopki */}
-	  <div className="absolute -top-20 left-1/2 transform -translate-x-1/2  p-4 rounded-full hover:scale-110 transition-transform duration-300">	  <Logo />
-      </div>
-      {/* Zawartość stopki */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 mx-auto mt-16 sm:max-w-[950px] gap-8 sm:gap-16 px-4">
-        {/* Sekcja po lewej */}
-        <div className="text-left">
-		<p className="font-semibold">Kontakt</p>
+	return (
+		<div id="contact" className="relative text-white font-quicksand px-4 bg-green-900 border-t-[1px] -mt-40 md:mt-24">
+			{/* Logo w górnej części stopki */}
+			<div className="absolute -top-20 left-1/2 transform -translate-x-1/2  p-4 rounded-full hover:scale-110 transition-transform duration-300">
+				{' '}
+				<Logo />
+			</div>
+			{/* Zawartość stopki */}
+			<div className="grid grid-cols-1 sm:grid-cols-2 mx-auto mt-16 sm:max-w-[950px] gap-8 sm:gap-16 px-4">
+				{/* Sekcja po lewej */}
+				<div className="text-left">
+					<p className="font-semibold">Kontakt</p>
 
-          <p className="mt-1">Mariusz Pięta</p>
-          <p className="mt-1">Tel: +48 661 647 437</p>
-          <p className="mt-1">Email: mar.pieta@wp.pl</p>
-        </div>
+					<p className="mt-1">Mariusz Pięta</p>
+					<p className="mt-1">Tel: +48 661 647 437</p>
+					<p className="mt-1">Email: dachypieta@gmail.com</p>
+				</div>
 
-        {/* Sekcja po prawej */}
-        <div className="text-left sm:text-right">
-          <p className="font-semibold">Adres:</p>
-          <p className="mt-1">Gnojnica 55</p>
-          <p className="mt-1">39-105 Gnojnica</p>
-        </div>
-      </div>
-      <section className="mx-auto">
-        <p className="text-center mt-8 text-[0.7rem] font-[500]">Created by: Łukasz Wilczyński</p>
-      </section>
-    </div>
-  )
+				{/* Sekcja po prawej */}
+				<div className="text-left sm:text-right">
+					<p className="font-semibold">Adres:</p>
+					<p className="mt-1">Gnojnica 55</p>
+					<p className="mt-1">39-105 Gnojnica</p>
+				</div>
+			</div>
+			<section className="mx-auto">
+				<p className="text-center mt-8 text-[0.7rem] font-[500]">Created by: Łukasz Wilczyński</p>
+			</section>
+		</div>
+	)
 }
 
 export default Footer
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,122 +4,142 @@ import { motion, AnimatePresence } from 'framer-motion'
 import Logo from './Logo'
 import { FaPhone } from 'react-icons/fa'
 import { IoIosMail } from 'react-icons/io'
-import { Link } from 'react-scroll'; // Import react-scroll
+import { Link } from 'react-scroll' // Import react-scroll
 
 const Navbar = () => {
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false)
+	const [isMobileMenuOpen, setMobileMenuOpen] = useState(false)
 
-  const toggleMobileMenu = () => {
-    setMobileMenuOpen(!isMobileMenuOpen)
-  }
+	const toggleMobileMenu = () => {
+		setMobileMenuOpen(!isMobileMenuOpen)
+	}
 
-  const closeMobileMenu = () => {
-    setMobileMenuOpen(false)
-  }
+	const closeMobileMenu = () => {
+		setMobileMenuOpen(false)
+	}
 
-  return (
-    <nav id="/" className="md:p-0 p-2 relative z-10">
-      {/* Desktop Navigation */}
-      <div className="hidden md:flex items-center justify-between">
-        <Logo />
+	return (
+		<nav id="/" className="md:p-0 p-2 relative z-10">
+			{/* Desktop Navigation */}
+			<div className="hidden md:flex items-center justify-between">
+				<Logo />
 
-        <div className="flex flex-col items-center justify-between space-y-2">
-          <span className="text-lg font-semibold text-black flex items-center">
-            <IoIosMail className="mr-2 text-green-400 text-2xl" />
-            mar.pieta@wp.pl
-          </span>
-          <span className="text-lg font-semibold text-black flex items-center">
-            <FaPhone className="mr-2 text-green-400" />
-            +48 661 647 437
-          </span>
-        </div>
+				<div className="flex flex-col items-center justify-between space-y-2">
+					<span className="text-lg font-semibold text-black flex items-center">
+						<IoIosMail className="mr-2 text-green-400 text-2xl" />
+						dachypieta@gmail.com
+					</span>
+					<span className="text-lg font-semibold text-black flex items-center">
+						<FaPhone className="mr-2 text-green-400" />
+						+48 661 647 437
+					</span>
+				</div>
 
-        <div className="md:space-x-5 lg:space-x-20 xxl:space-x-40 text-2xl mr-20 font-semibold">
-          <Link to="start" smooth={true} duration={500} className="nav-link text-green-400 cursor-pointer">
-            Start
-          </Link>
-          <Link to="aboutus" smooth={true} duration={500} className="nav-link text-black cursor-pointer">
-            O nas
-          </Link>
-          <Link to="realizations" smooth={true} duration={500} className="nav-link text-black cursor-pointer">
-            Realizacje
-          </Link>
-          <Link to="offers" smooth={true} duration={500} className="nav-link text-black cursor-pointer">
-            Oferta
-          </Link>
-          <Link to="contact" smooth={true} duration={500} className="nav-link text-black cursor-pointer">
-            Kontakt
-          </Link>
-        </div>
-      </div>
+				<div className="md:space-x-5 lg:space-x-20 xxl:space-x-40 text-2xl mr-20 font-semibold">
+					<Link to="start" smooth={true} duration={500} className="nav-link text-green-400 cursor-pointer">
+						Start
+					</Link>
+					<Link to="aboutus" smooth={true} duration={500} className="nav-link text-black cursor-pointer">
+						O nas
+					</Link>
+					<Link to="realizations" smooth={true} duration={500} className="nav-link text-black cursor-pointer">
+						Realizacje
+					</Link>
+					<Link to="offers" smooth={true} duration={500} className="nav-link text-black cursor-pointer">
+						Oferta
+					</Link>
+					<Link to="contact" smooth={true} duration={500} className="nav-link text-black cursor-pointer">
+						Kontakt
+					</Link>
+				</div>
+			</div>
 
-      {/* Mobile Navigation */}
-      <div className="md:hidden flex items-center justify-between">
-        <Logo className="w-2/3" />
+			{/* Mobile Navigation */}
+			<div className="md:hidden flex items-center justify-between">
+				<Logo className="w-2/3" />
 
-        <motion.button
-          className="p-2 focus:outline-none text-3xl text-green-400"
-          onClick={toggleMobileMenu}
-          initial={false}
-          animate={{ rotate: isMobileMenuOpen ? 45 : 0 }}
-          transition={{ duration: 0.3, ease: 'easeInOut' }}>
-          ☰
-        </motion.button>
-      </div>
+				<motion.button
+					className="p-2 focus:outline-none text-3xl text-green-400"
+					onClick={toggleMobileMenu}
+					initial={false}
+					animate={{ rotate: isMobileMenuOpen ? 45 : 0 }}
+					transition={{ duration: 0.3, ease: 'easeInOut' }}>
+					☰
+				</motion.button>
+			</div>
 
-      {/* Mobile Menu */}
-      <AnimatePresence>
-        {isMobileMenuOpen && (
-          <motion.div
-            key="mobileMenu"
-            initial={{ opacity: 0, x: '-100%' }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: '-100%' }}
-            transition={{ duration: 0.3, ease: 'easeInOut' }}
-            className="md:hidden ml-5 absolute top-0 right-0 bg-gray-300 bg-opacity-10 h-screen backdrop-blur-md w-full z-10">
-            <div className="flex items-center justify-between p-8">
-              <motion.button
-                className="text-black focus:outline-none text-3xl relative left-full top-8"
-                onClick={closeMobileMenu}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.3, ease: 'easeInOut' }}>
-                ✕
-              </motion.button>
-            </div>
-            <div className="space-y-16 font-semibold h-full flex flex-col items-center text-lg pb-36">
-              <RouterLink to="/" className="text-green-400 cursor-pointer" onClick={closeMobileMenu}>
-                START
-              </RouterLink>
-              <Link to="aboutus" smooth={true} duration={500} className="text-white cursor-pointer" onClick={closeMobileMenu}>
-                O NAS
-              </Link>
-              <Link to="realizations" smooth={true} duration={500} className="text-white cursor-pointer" onClick={closeMobileMenu}>
-                REALIZACJE
-              </Link>
-              <Link to="offers" smooth={true} duration={500} className="text-white cursor-pointer" onClick={closeMobileMenu}>
-                OFERTA
-              </Link>
-              <Link to="contact" smooth={true} duration={500} className="text-white cursor-pointer" onClick={closeMobileMenu}>
-                KONTAKT
-              </Link>
-              <div className="flex flex-col items-center justify-between space-y-4">
-                <span className="text-lg font-semibold text-white flex items-center">
-                  <IoIosMail className="mr-2 text-green-400 text-2xl" />
-                  mar.pieta@wp.pl
-                </span>
-                <span className="text-lg font-semibold text-white flex items-center">
-                  <FaPhone className="mr-2 text-green-400" />
-                  +48 661 647 437
-                </span>
-              </div>
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </nav>
-  )
+			{/* Mobile Menu */}
+			<AnimatePresence>
+				{isMobileMenuOpen && (
+					<motion.div
+						key="mobileMenu"
+						initial={{ opacity: 0, x: '-100%' }}
+						animate={{ opacity: 1, x: 0 }}
+						exit={{ opacity: 0, x: '-100%' }}
+						transition={{ duration: 0.3, ease: 'easeInOut' }}
+						className="md:hidden ml-5 absolute top-0 right-0 bg-gray-300 bg-opacity-10 h-screen backdrop-blur-md w-full z-10">
+						<div className="flex items-center justify-between p-8">
+							<motion.button
+								className="text-black focus:outline-none text-3xl relative left-full top-8"
+								onClick={closeMobileMenu}
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1 }}
+								exit={{ opacity: 0 }}
+								transition={{ duration: 0.3, ease: 'easeInOut' }}>
+								✕
+							</motion.button>
+						</div>
+						<div className="space-y-16 font-semibold h-full flex flex-col items-center text-lg pb-36">
+							<RouterLink to="/" className="text-green-400 cursor-pointer" onClick={closeMobileMenu}>
+								START
+							</RouterLink>
+							<Link
+								to="aboutus"
+								smooth={true}
+								duration={500}
+								className="text-white cursor-pointer"
+								onClick={closeMobileMenu}>
+								O NAS
+							</Link>
+							<Link
+								to="realizations"
+								smooth={true}
+								duration={500}
+								className="text-white cursor-pointer"
+								onClick={closeMobileMenu}>
+								REALIZACJE
+							</Link>
+							<Link
+								to="offers"
+								smooth={true}
+								duration={500}
+								className="text-white cursor-pointer"
+								onClick={closeMobileMenu}>
+								OFERTA
+							</Link>
+							<Link
+								to="contact"
+								smooth={true}
+								duration={500}
+								className="text-white cursor-pointer"
+								onClick={closeMobileMenu}>
+								KONTAKT
+							</Link>
+							<div className="flex flex-col items-center justify-between space-y-4">
+								<span className="text-lg font-semibold text-white flex items-center">
+									<IoIosMail className="mr-2 text-green-400 text-2xl" />
+									dachypieta@gmail.com
+								</span>
+								<span className="text-lg font-semibold text-white flex items-center">
+									<FaPhone className="mr-2 text-green-400" />
+									+48 661 647 437
+								</span>
+							</div>
+						</div>
+					</motion.div>
+				)}
+			</AnimatePresence>
+		</nav>
+	)
 }
 
 export default Navbar
